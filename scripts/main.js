fbd76fded04b22c174af18c6026fbc6f04238974
joyxy <jz442@cornell.edu>
2025-03-06 11:08:19 -0500

    Lock scrolling in fullscreen image

--- scripts/main.js
+++ scripts/main.js
@@ -4,14 +4,20 @@ function showFullscreen(imgElement) {
 
     fullscreenImage.src = imgElement.src;
     fullscreenHolder.style.display = "flex";
+
+    document.documentElement.style.overflow = "hidden";
+    document.body.style.overflow = "hidden";
 }
 
 function hideFullscreen() {
     document.getElementById("fullscreen-holder").style.display = "none";
+
+    document.documentElement.style.overflow = "auto";
+    document.body.style.overflow = "auto";
 }
 
-document.querySelectorAll('.project-image').forEach(image => {
-    image.addEventListener('click', function() {
+document.querySelectorAll(".project-image").forEach(image => {
+    image.addEventListener("click", function() {
         showFullscreen(this);
     });
 });
